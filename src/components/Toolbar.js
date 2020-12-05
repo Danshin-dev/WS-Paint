8a7f272d7c72c2dc9896e88bdfe1a660038058cd
Dmitrii Danshin <dmitrii.danshin@timelysoft.net>
2020-12-05 18:37:18 +0600

    Drawing rectangle

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "../styles/toolbar.scss";
 import toolState from "../store/toolState";
-import Brush from "../tools/Brush";
 import canvasState from "../store/canvasState";
+import Brush from "../tools/Brush";
+import Rectangle from "../tools/Rectangle";
 
 const Toolbar = () => {
   return (
@@ -11,7 +12,10 @@ const Toolbar = () => {
         className="toolbar__btn brush"
         onClick={() => toolState.setTool(new Brush(canvasState.canvas))}
       />
-      <button className="toolbar__btn square" />
+      <button
+        className="toolbar__btn square"
+        onClick={() => toolState.setTool(new Rectangle(canvasState.canvas))}
+      />
       <button className="toolbar__btn circle" />
       <button className="toolbar__btn eraser" />
       <button className="toolbar__btn line" />
